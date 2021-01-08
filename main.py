fce11143717b15bc88103be89c0f9cb7840b1629
Caleb <caleb.farrell987@gmail.com>
2021-01-08 18:37:24 -0400

    Added functionality to compress contents to the /var/logs folder. NOTE: this requires you to modify the permissions of the var/logs directory recursively to rw for all. I will work in a work around in the future. (Probably can be resolved by running the script as a super user!)

--- main.py
+++ main.py
@@ -5,34 +5,47 @@ import logging
 from subprocess import Popen, PIPE, STDOUT, call
 from pathlib import Path
 from os.path import expanduser
+import tarfile
 
+
+pLogs = "/var/log/"
+pSysLogs = glob.glob(pLogs + "*")
+pStageDir = str(Path.home()) + '/tmp'
 
 
 def getSysLogs():
-    pLogs = "/var/log/"
-    pSyslogs = glob.glob(pLogs + "*")
-    pStageDir =  str(Path.home()) + '/tmp'
+    getpSysLogs =pSysLogs
+    getpStageDir = pStageDir
     indexListArray = []
 
-    if bool(os.path.exists(pStageDir)) != True:
+    if bool(os.path.exists(getpStageDir)) != True:
 
         try:
-            os.mkdir(pStageDir)
+            os.mkdir(getpStageDir)
         except OSError:
-            print("Creation of Directory %s failed" % pStageDir)
+            print("Creation of Directory %s failed" % getpStageDir)
         else:
-            print("Created Temporary directory %s " % pStageDir)
+            print("Created Temporary directory %s " % getpStageDir)
 
     else:
-        print("Success! " + "Directory: " + "'" + pStageDir + "'" + " Exists!")
+        print("Success! " + "Directory: " + "'" + getpStageDir + "'" + " Exists!")
 
-    for item in pSyslogs:
+    for item in getpSysLogs:
         #print(item)
         indexListArray.append(item)
-        indexFile = open(pStageDir + "/index.txt", "a")
+        indexFile = open(getpStageDir + "/index.txt", "a")
         #print(indexListArray)
         indexFile.writelines("%s\n" % line for line in indexListArray)
 
+
+def compress_logs ():
+    getpSysLogs = pSysLogs
+    getindexListArray = getpSysLogs
+    tar = tarfile.open(pStageDir + "/logs-backup.tar.gz", "w:gz")
+    for item in getindexListArray:
+        print("  Adding %s..." % item)
+        tar.add(item, os.path.basename(item))
+    tar.close()
 
 def print_welcome(name):
     print(f'Hi, {name}')
@@ -42,3 +55,4 @@ def print_welcome(name):
 if __name__ == '__main__':
     print_welcome(getpass.getuser())
     getSysLogs()
+    compress_logs()
