0e158aecb88af303369c9780f74e50cb9587d5a6
Caleb <caleb.farrell987@gmail.com>
2021-01-08 19:09:22 -0400

    Added functionality to compress contents to the /var/logs folder. NOTE: this requires you to modify the permissions of the var/logs directory recursively to rw for all. I will work in a work around in the future. (Probably can be resolved by running the script as a super user!)
    Added Comments

--- main.py
+++ main.py
@@ -7,51 +7,64 @@ from pathlib import Path
 from os.path import expanduser
 import tarfile
 
-
+# TODO Logging
+# TODO json or cfg to define paths and variable switches such as logging.
+# Defines the paths needed
+# Path to the logs folder
 pLogs = "/var/log/"
-pSysLogs = glob.glob(pLogs + "*")
-pStageDir = str(Path.home()) + '/tmp'
+# Gobbing all the sub directories together to collect them all in one place
+p_sys_logs = glob.glob(pLogs + "*")
+# this directory is where the compressed System logs and python logs will be stored
+p_stage_dir = str(Path.home()) + '/staging'
 
 
+# This function gets the system logs paths and stores them in a dict to be used later on.
+# It also creates the staging directory for the script in.
 def getSysLogs():
-    getpSysLogs =pSysLogs
-    getpStageDir = pStageDir
-    indexListArray = []
+    get_sys_logs = p_sys_logs
+    get_stage_dir = p_stage_dir
+    index_list_array = []
 
-    if bool(os.path.exists(getpStageDir)) != True:
+# Checks if the Staging directory doesn't exist, I know this is backwards but like, deal with it
+    if bool(os.path.exists(get_stage_dir)) != True:
 
+        # Try/Catch because Errors
         try:
-            os.mkdir(getpStageDir)
+            os.mkdir(get_stage_dir)
         except OSError:
-            print("Creation of Directory %s failed" % getpStageDir)
+            print("Creation of Directory %s failed" % get_stage_dir)
         else:
-            print("Created Temporary directory %s " % getpStageDir)
-
+            print("Created Temporary directory %s " % get_stage_dir)
+    # Success print if the dir already exists
     else:
-        print("Success! " + "Directory: " + "'" + getpStageDir + "'" + " Exists!")
+        print("Success! " + "Directory: " + "'" + get_stage_dir + "'" + " Exists!")
 
-    for item in getpSysLogs:
-        #print(item)
-        indexListArray.append(item)
-        indexFile = open(getpStageDir + "/index.txt", "a")
-        #print(indexListArray)
-        indexFile.writelines("%s\n" % line for line in indexListArray)
+    # Writes each directory to the index_list_array dict
+    for item in get_sys_logs:
+        # print(item)
+        index_list_array.append(item)
+        indexFile = open(get_stage_dir + "/index.txt", "a")
+        # print(index_list_array)
+        indexFile.writelines("%s\n" % line for line in index_list_array)
 
 
-def compress_logs ():
-    getpSysLogs = pSysLogs
-    getindexListArray = getpSysLogs
-    tar = tarfile.open(pStageDir + "/logs-backup.tar.gz", "w:gz")
-    for item in getindexListArray:
+# Packages and compresses the System Logs and stores the tar.gz in the staging folder
+def compress_logs():
+    getp_sys_logs = p_sys_logs
+    getindex_list_array = getp_sys_logs
+    tar = tarfile.open(p_stage_dir + "/logs-backup.tar.gz", "w:gz")
+    for item in getindex_list_array:
         print("  Adding %s..." % item)
         tar.add(item, os.path.basename(item))
     tar.close()
 
+
+# Welcomes the current user as the script starts
 def print_welcome(name):
     print(f'Hi, {name}')
 
 
-
+# Main Method
 if __name__ == '__main__':
     print_welcome(getpass.getuser())
     getSysLogs()
