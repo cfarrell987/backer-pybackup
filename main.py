531a25cf21f61cf515af41cf5e72b64ede6b9db2
Caleb <caleb.farrell987@gmail.com>
2021-01-10 23:04:01 -0400

    Added logging functionality, Some Refactoring and renaming of variables to better reflect their functions.

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import getpass
 import os
 import glob
+# noinspection PyUnresolvedReferences
 import logging
-import subprocess
 from pathlib import Path
-from os.path import expanduser
 import tarfile
 import shutil
 from datetime import date
+import time
 import configparser
 
-# TODO Logging
-# TODO json or cfg to define paths and variable switches such as logging.
+# DONE Logging
+# DONE json or cfg to define paths and variable switches such as logging.
 # DONE create function that uploads logs tar to CDN (Private folder)
 # TODO in soon-to-be-made config file, add option to change destination and share type i.e NFS, SMB, FTP etc.
 
-### Defines the paths needed ###
-
-# Path to the logs folder
-pLogs = "/var/log/"
 
 # Define today
 today = date.today()
@@ -28,6 +24,7 @@ compressed_logs_name = "/logs-backup" + today.strftime("%b-%d-%Y") + ".tar.gz"
 
 
 def cfgparse():
+    # Define path to system logs
     global logsPath
     # this directory is where the compressed System logs and python logs will be stored
     global stagingPath
@@ -48,74 +45,84 @@ def cfgparse():
     stagingPath = str(Path.home()) + config['PATHS']['stagingPath']
     upload_dest = config['PATHS']['uploadDestinationPath']
     p_sys_logs = glob.glob(str(logsPath) + "/*", recursive=True)
-    logging_bool = config['OPTIONS']['logging']
-    index_bool = config['OPTIONS']['index']
+    logging_bool = config.getboolean('OPTIONS', 'logging')
+    index_bool = config.getboolean('OPTIONS', 'index')
+
+
+def logger():
+    if logging_bool is True:
+        print("Initializing Logging...")
+        if os.path.exists(stagingPath + time.strftime("/%Y-%m-%d") + 'logs.log'):
+            os.remove(stagingPath + time.strftime("/%Y-%m-%d") + 'logs.log')
+        logging.basicConfig(format='%(asctime)s %(message)s', datefmt='\%m/%d/%Y %I:%M:%S %p', filename=stagingPath +
+                time.strftime("/%Y-%m-%d") + 'logs.log', level=logging.DEBUG)
+        print("Complete! ")
+    else:
+        logging.warning('Logging is Off.')
+
 
 # This function gets the system logs paths and stores them in a dict to be used later on.
 # It also creates the staging directory for the script in.
 def get_sys_logs():
     index_list_array = []
 
-    # Checks if the Staging directory doesn't exist, I know this is backwards but like, deal with it
-    if bool(os.path.exists(stagingPath)) != True:
+    if bool(os.path.exists(stagingPath)) is not True:
 
         # Try/Catch because Errors
         try:
             os.mkdir(stagingPath)
         except OSError:
-            print("Creation of Directory %s failed" % stagingPath)
+            logging.error("Error: " "Creation of Directory %s failed" % stagingPath)
         else:
-            print("Created Temporary directory %s " % stagingPath)
+            logging.info("Created Temporary directory %s " % stagingPath)
     # Success print if the dir already exists
     else:
-        print("Success! " + "Directory: " + "'" + stagingPath + "'" + " Exists!")
+        logging.info("Success! " + "Directory: " + "'" + stagingPath + "'" + " Exists!")
 
+    if index_bool is not True:
+        logging.warning("Index Logging is off!")
     # Writes each directory to the index_list_array dict
-    if index_bool == "ON":
+    else:
         for item in p_sys_logs:
             index_list_array.append(item)
             index_file = open(stagingPath + "/index.txt", "a")
             index_file.writelines("%s\n" % line for line in index_list_array)
-    else:
-        print("Index Logging is OFF")
+
 
 # Packages and compresses the System Logs and stores the tar.gz in the staging folder
 def compress_logs():
-    getp_sys_logs = p_sys_logs
-    getindex_list_array = getp_sys_logs
-    getcompressed_logs_name = compressed_logs_name
     tar = tarfile.open(stagingPath + compressed_logs_name, "w:gz")
-    for item in getindex_list_array:
+    for item in p_sys_logs:
         print("  Adding %s..." % item)
         tar.add(item, os.path.basename(item))
     tar.close()
 
 
 def upload_logs():
-    if bool(os.path.exists(os.path.join(upload_dest, "/Private"))) != True:
-        print(os.path.join(upload_dest + "/Private"))
+    if bool(os.path.exists(os.path.join(upload_dest))) is not True:
+        print(os.path.join(upload_dest))
         # subprocess.run("mount", "-t", "cifs", "-o", "credentials=/$HOME/creds/smb.creds", "//192.168.2.225/D",
         # "/mnt/samba" )
     else:
         shutil.copyfile(stagingPath + compressed_logs_name,
-                        upload_dest + os.path.join("/Private" + compressed_logs_name))
-        print("Success! " + "logs backed up!")
+                        upload_dest + os.path.join(compressed_logs_name))
+        logging.info("Success! " + "logs backed up to: " + upload_dest)
 
 
 def clean_staging():
-    print("Cleaning all data from staging directory")
+    logging.info("Cleaning all data from staging directory")
     try:
         os.remove(stagingPath + compressed_logs_name)
-        print("tar ball deleted!")
+        logging.info("tar ball deleted!")
     except OSError as e:
-        print("Error: %s - %s." % (e.filename, e.strerror))
+        logging.error("Error: %s - %s." % (e.filename, e.strerror))
     else:
         pass
     try:
         os.remove(stagingPath + "/index.txt")
-        print("index deleted!")
+        logging.info("index deleted!")
     except OSError as e:
-        print("Error: %s - %s." % (e.filename, e.strerror))
+        logging.error("Error: %s - %s." % (e.filename, e.strerror))
     else:
         pass
 
@@ -129,7 +136,8 @@ def print_welcome(name):
 if __name__ == '__main__':
     print_welcome(getpass.getuser())
     cfgparse()
+    logger()
     get_sys_logs()
-    #compress_logs()
-    #upload_logs()
-    #clean_staging()
+    # compress_logs()
+    upload_logs()
+    # clean_staging()
