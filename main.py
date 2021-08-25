096cfe77a49d73fa40c6c3f832ae349db751eda1
cfarrell987 <caleb.farrell987@gmail.com>
2021-08-24 21:52:50 -0300

    Following some incredible code review, Some significant changes have been made with more to come!
    ADDED:
    - Function to get the current logged in user, this prevents the home path being set to /root when run as SUDO
    - Function to set the home path based on OS settings, allows for more portability
    
    CHANGES:
    - Refactored somes variable to match snake_case with rest of script
    - Changed compressed_logs_name to remove the absolute path
    - staging_path now gets its home path from the get_home_path function
    - logger() refactored to initialize_logging() to better give an idea of what it does at a glance
    - Opening the tarfile now with 100% less chance of failing!
    - fixed issue with uploading logs would fail, i.e I made an oopsy and didn't notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import getpass
 import os
+import pwd
 import subprocess
 import glob
 import logging
@@ -22,15 +23,15 @@ import configparser
 today = date.today()
 
 # Define the file name for the tar-ball of logs
-compressed_logs_name = "/logs-backup" + today.strftime("%b-%d-%Y") + ".tar.gz"
+compressed_logs_name = "logs-backup" + today.strftime("%b-%d-%Y") + ".tar.gz"
 
 
 def cfgparse():
     # Define path to system logs
-    global logsPath
+    global logs_path
     # this directory is where the compressed System logs and python
     # logs will be stored
-    global stagingPath
+    global staging_path
     # Define the upload destination for the tar-ball
     global upload_dest
     # Gobbing all the sub directories together to collect them all in one place
@@ -45,31 +46,31 @@ def cfgparse():
     config.read('init.INI')
     config.sections()
 
-    logsPath = config['PATHS']['logsPath']
-    stagingPath = str(Path.home()) + config['PATHS']['stagingPath']
-    upload_dest = config['PATHS']['uploadDestinationPath']
-    p_sys_logs = glob.glob(str(logsPath) + "/*", recursive=True)
+    logs_path = config['PATHS']['logs_path']
+    staging_path = str(get_home_path()) + config['PATHS']['staging_path']
+    upload_dest = config['PATHS']['upload_destination_path']
+    p_sys_logs = glob.glob(str(logs_path) + "/*", recursive=True)
     logging_bool = config.getboolean('OPTIONS', 'logging')
     index_bool = config.getboolean('OPTIONS', 'index')
 
 
-def logger():
+def initialize_logging():
     curr_path = os.path.dirname(os.path.realpath(__file__))
-    loggingPath = str(curr_path) + '/logs'
+    logging_path = str(curr_path) + '/logs'
 
     if logging_bool is True:
-        if bool(os.path.exists(loggingPath)) is not True:
+        if bool(os.path.exists(logging_path)) is not True:
             try:
-                os.mkdir(loggingPath)
+                os.mkdir(logging_path)
                 print("Logs Directory Created!")
             except OSError:
-                print(f'Cannot create Logs directory at %s' % loggingPath)
+                print(f'Cannot create Logs directory at %s' % logging_path)
         print("Initializing Logging...")
-        if os.path.exists(stagingPath + time.strftime("/%Y-%m-%d") +
+        if os.path.exists(staging_path + time.strftime("/%Y-%m-%d") +
                 'logs.log'):
-            os.remove(stagingPath + time.strftime("/%Y-%m-%d") + 'logs.log')
+            os.remove(staging_path + time.strftime("/%Y-%m-%d") + 'logs.log')
         logging.basicConfig(format='%(asctime)s %(message)s', 
-                datefmt='\%m/%d/%Y %I:%M:%S %p', filename=loggingPath 
+                datefmt='\%m/%d/%Y %I:%M:%S %p', filename=logging_path 
                 + time.strftime("/%Y-%m-%d") + 'logs.log', level=logging.DEBUG)
 
         print("Complete! ")
@@ -82,19 +83,19 @@ def logger():
 def get_sys_logs():
     index_list_array = []
 
-    if bool(os.path.exists(stagingPath)) is not True:
+    if bool(os.path.exists(staging_path)) is not True:
 
         # Try/Catch because Errors
         try:
-            os.mkdir(stagingPath)
+            os.mkdir(staging_path)
         except OSError:
             logging.error("Error: " "Creation of Directory %s failed" % 
-                stagingPath)
+                staging_path)
         else:
-            logging.info("Created Temporary directory %s " % stagingPath)
+            logging.info("Created Temporary directory %s " % staging_path)
     # Success print if the dir already exists
     else:
-        logging.info("Success! " + "Directory: " + "'" + stagingPath + "'" + 
+        logging.info("Success! " + "Directory: " + "'" + staging_path + "'" + 
             "Exists!")
 
     if index_bool is not True:
@@ -103,17 +104,22 @@ def get_sys_logs():
     else:
         for item in p_sys_logs:
             index_list_array.append(item)
-            index_file = open(stagingPath + "/index.txt", "a")
+            index_file = open(staging_path + "/index.txt", "a")
             index_file.writelines("%s\n" % line for line in index_list_array)
 
 
 # Packages and compresses System Logs, stores the tar.gz in the staging folder
 def compress_logs():
-    tar = tarfile.open(stagingPath + compressed_logs_name, "w:gz")
-    for item in p_sys_logs:
-        print("  Adding %s..." % item)
-        tar.add(item, os.path.basename(item))
-    tar.close()
+    try:
+        print(os.path.join(staging_path, compressed_logs_name))
+        tar = tarfile.open(os.path.join(staging_path, compressed_logs_name), "w:gz")
+    except:
+        logging.error("ERROR: failed to open tarfile!")
+    finally:
+        for item in p_sys_logs:
+            print("  Adding %s..." % item)
+            tar.add(item, os.path.basename(item))
+        tar.close()
 
 
 def upload_logs():
@@ -123,8 +129,8 @@ def upload_logs():
     else:
         logging.info("Share Mounted!")
     try:
-        shutil.copyfile(stagingPath + compressed_logs_name,
-                        upload_dest + os.path.join(compressed_logs_name))
+        shutil.copyfile(os.path.join(staging_path, compressed_logs_name),
+                        os.path.join(upload_dest, compressed_logs_name))
         logging.info("Success! " + "logs backed up to: " + upload_dest)
     except:
         logging.error('Error: CANNOT COPY FILES!')
@@ -135,15 +141,15 @@ def upload_logs():
 def clean_staging():
     logging.info("Cleaning all data from staging directory")
     try:
-        os.remove(stagingPath + compressed_logs_name)
-        print(stagingPath)
-        logging.info("tar ball deleted!")
+       # os.remove(staging_path + compressed_logs_name)
+        print(staging_path)
+       # logging.info("tar ball deleted!")
     except OSError as e:
         logging.error("Error: %s - %s." % (e.filename, e.strerror))
     else:
         pass
     try:
-        os.remove(stagingPath + "/index.txt")
+        os.remove(staging_path + "/index.txt")
         logging.info("index deleted!")
     except OSError as e:
         logging.error("Error: %s - %s." % (e.filename, e.strerror))
@@ -151,16 +157,51 @@ def clean_staging():
         pass
 
 
-# Welcomes the current user as the script starts
-def print_welcome(name):
-    print(f'Hi, {name}')
+
+#Gets the current logged in user
+#This is required as if the script is run with sudo
+#the ~ will be set to sudo
+def get_logged_user():
+
+    try:
+        return os.getlogin()
+    except:
+        pass
+
+    try:
+        user = os.environ['USER']
+    except KeyError:
+        return getpass.getuser()
+
+    if user == 'root':
+        try:
+            return os.environ['SUDO_USER']
+        except KeyError:
+            pass
+
+        try:
+            pkexec_uid = int(os.environ['PKEXEC_UID'])
+            return owd.getpwuid(pkexec_uid).pw_name
+        except KeyError:
+            pass
+    return user
+
+#Sets home_dir to whatever the logged in users home path is
+#Needs to be done this way in case a user decides to 
+#change their ~ to anything other than /home/$USER
+#should also allow portability with MacOS
+def get_home_path():
+    home_dir = pwd.getpwnam(get_logged_user()).pw_dir
+    return home_dir
 
 
 # Main Method
 if __name__ == '__main__':
-    print_welcome(getpass.getuser())
+
+    get_logged_user()
+    get_home_path()
     cfgparse()
-    logger()
+    initialize_logging()
     get_sys_logs()
     compress_logs()
     upload_logs()
